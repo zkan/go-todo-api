6010607418147c967985128452b9b1e20228fe10
zkan <zkan.cs@gmail.com>
2022-03-05 14:44:14 +0700

    Create post API for creating new task

--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+
+	"github.com/zkan/go-todo-api/todo"
+)
 
 func main() {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&todo.Todo{})
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
+	handler := todo.NewTodoHandler(db)
+	r.POST("/todos", handler.NewTask)
+
 	r.Run()
 }
